1d46388004f30c34786fce857ddbeb22cf0f9d81
Kevin Smith <smithkm@draconic.ca>
2024-12-25 16:27:17 -0800

    Build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+fontforge  -lang=ff -c 'Open($1); Generate($2)' scriven.sfd Scriven.otf
+fontforge  -lang=ff -c 'Open($1); Generate($2)' scriven.sfd Scriven.woff2
+cp Scriven.otf ~/.local/share/fonts/
+cp Scriven.woff2 ../smithkm.github.io/runes/
+#fc-cache -f -v
+fontimage \
+    --pixelsize 48 \
+    --text "Scriven" \
+    --pixelsize 32 \
+    --text "0123456789!?(){}[]& ◇‍[ ]‍◇ ¶" \
+    --text "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz" \
+    --text "ᚠᚡᚢᚣᚤᚥᚦᚧᚨᚩᚪᚫᚬᚭᚮᚯᚰᚱᚲᚳᚴᚵᚶᚷᚸᚹᚺᚻᚼᚽᚾᚿᛀᛁᛂᛃᛄᛅᛆᛇᛈᛉᛊᛋᛌᛍᛎᛏᛐᛑᛒᛓᛔᛕᛖᛗᛘᛙᛚᛛᛜᛝᛞᛟᛠᛡᛢᛣᛤᛥᛦᛧᛨᛩᛪᛱᛲᛳᚢ︆ ᛫︆᛬᛭" \
+    --text "  " \
+    --text "∴" \
+    --text "Sphinx of black quartz, heedst thou my pleading and judgest my vow." \
+    --text "◇‍[ᛋᛕᚹᛁᚾᛣ᛫ᚩᚠ᛫ᛒ︀ᛚᚫᚳᛱ᛫ᚴᚢᚫᚱ︀ᛏᛉ᛫ᚹᛝᛞᛥ᛫ᚦᚩᚢ᛫ᛗᚣ᛫ᛕᛚᛠᛞᛁᛟ᛫ᚫᚾᛞ᛫ᛄᚢᛞᚷᛖᛥ᛫ᛗᚣ᛫ᚢ︆ᚩᚻ]‍◇" \
+    --text "           " \
+    --text "           " \
+    --text "󱥂󱤴󱤧󱤑󱦐K󱦒e󱦒w󱦒i󱦒n󱦒󱦑。󱤿󱤆󱤧󱥔󱥩󱤑󱤆。" \
+Scriven.otf
